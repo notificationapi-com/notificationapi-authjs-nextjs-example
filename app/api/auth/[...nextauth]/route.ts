9ad132bfd6b4da55928c6f11c912e0bccc61021a
Sahand Seifi <sahand.seifi@gmail.com>
2025-02-19 18:14:49 -0500

    Implemented using NotificationAPI

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,7 @@ import EmailProvider from "next-auth/providers/email";
 import { AuthOptions } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import notificationapi from "notificationapi-node-server-sdk";
 
 const prisma = new PrismaClient();
 
@@ -10,8 +11,22 @@ export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     EmailProvider({
-      server: "smpt_server",
-      from: "from_address",
+      async sendVerificationRequest({ identifier: email, url }) {
+        notificationapi.init(
+          process.env.NOTIFICATIONAPI_CLIENT_ID as string,
+          process.env.NOTIFICATIONAPI_CLIENT_SECRET as string
+        );
+        await notificationapi.send({
+          notificationId: "magic_link", // Create this template in NotificationAPI dashboard
+          user: {
+            id: email,
+            email: email,
+          },
+          mergeTags: {
+            magic_link: url,
+          },
+        });
+      },
     }),
   ],
 };
